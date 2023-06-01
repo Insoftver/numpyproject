5dabb86a773ef0efbc4de8e1089185de9401c167
mota_diego <d.insoftver@gmail.com>
2023-06-01 14:33:09 -0600

    Added start.html

--- matrix/views.py
+++ matrix/views.py
@@ -7,4 +7,4 @@ def sum_matrix(request):
     a = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
     b = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
     c = a + b
-    return HttpResponse(c)
+    return render(request, 'matrix/start.html', {'c': c})
