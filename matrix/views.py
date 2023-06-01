69d2fee7d934bab33cddd1d1b1ac9f4e4d012a83
mota_diego <d.insoftver@gmail.com>
2023-06-01 16:21:10 -0600

    Added Methods

--- matrix/views.py
+++ matrix/views.py
@@ -4,7 +4,56 @@ from django.http import HttpResponse
 
 def sum_matrix(request):
     import numpy as np
-    a = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-    b = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-    c = a + b
-    return render(request, 'matrix/start.html', {'c': c})
+    matrix1 = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    matrix2 = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+    addition = matrix1 + matrix2
+    subtract = matrix1 - matrix2
+    multiply = matrix1 * matrix2
+    divide = matrix1 / matrix2
+
+    shape = matrix1.shape
+    length = len(matrix1)
+    number_of_elements = matrix1.size
+    dimensions = matrix1.ndim
+
+    exponentiation = np.exp(matrix1)
+    square_root = np.sqrt(matrix1)
+    sin = np.sin(matrix1)
+    cos = np.cos(matrix1)
+    log = np.log(matrix1)
+
+    wise_sum = matrix1.sum()
+    wise_minimum_value = matrix1.min()
+    maximum_value_of_a_row = matrix1.max(axis=0)
+    cumulative_sum_of_a_row = matrix1.cumsum(axis=1)
+    mean = matrix1.mean(axis=0)
+    median = np.median(matrix1)
+    correlation_coeficient = np.corrcoef(matrix1)
+    standard_deviation = matrix1.std()
+
+    return render(request, 'matrix/start.html', {
+        'matrix1': matrix1,
+        'matrix2': matrix2,
+        'matrix_shape': shape,
+        'matrix_length': length,
+        'matrix_number_of_elements': number_of_elements,
+        'matrix_dimensions': dimensions,
+        'matrix_addition': addition,
+        'matrix_subtract': subtract,
+        'matrix_multiply': multiply,
+        'matrix_divide': divide,
+        'matrix_exponentiation': exponentiation,
+        'matrix_square_root': square_root,
+        'matrix_sin': sin,
+        'matrix_cos': cos,
+        'matrix_log': log,
+        'matrix_wise_sum': wise_sum,
+        'matrix_wise_minimum_value': wise_minimum_value,
+        'matrix_maximum_value_of_a_row': maximum_value_of_a_row,
+        'matrix_cumulative_sum_of_a_row': cumulative_sum_of_a_row,
+        'matrix_mean': mean,
+        'matrix_median': median,
+        'matrix_correlation_coeficient': correlation_coeficient,
+        'matrix_standard_deviation': standard_deviation,
+    })
--- numpyproject/urls.py
+++ numpyproject/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from matrix.views import sum_matrix
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', sum_matrix),
+    path('', include('matrix.urls')),
 ]
